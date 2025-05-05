c8d18e02ea4f7858b3823d9fde417ecee7405473
allistill12 <allisonstill@utexas.edu>
2025-05-05 16:21:29 -0500

    updates to projects

--- src/app/data/projectsData.js
+++ src/app/data/projectsData.js
@@ -4,21 +4,21 @@ export const projects = [
       description: "A full stack resource platform for single parents in the Austin area, providing affordable housing, nearby childcare, and books.",
       technologies: ["React", "Flask", "PostgreSQL", "RESTful APIs", "D3.js", "HTML/CSS"],
       image: "/parentAidATX.png",
-      link: "#"
+      link: "https://github.com/allisonstill/ParentAidATX-Single-Parent-Resources-in-Austin-"
     },
     {
       title: "Austin Animal Shelter Outcome Prediction Framework",
       description: "A multi-class classification model used to predict animal shelter outcomes for one of the largest no-kill shelters in the U.S.",
       technologies: ["XGBoost", "Neural Networks", "SMOTE Sampling"],
       image: "/austinAnimalCenter.jpg",
-      link: "#"
+      link: "https://github.com/allisonstill/AustinAnimalCenterPredictiveFramework"
     },
     {
       title: "More Projects on GitHub",
       description: "See more of my projects featuring machine learning and full-stack development on GitHub",
       technologies: ["JavaScript", "Python", "C"],
       image: "/github.jpg",
-      link: "#",
+      link: "https://github.com/allisonstill",
       miniProjects: [
         {
           name: "Proj1",
