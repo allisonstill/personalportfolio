631f6fed916addb92a7b3c00c253c661ec1c00b7
allistill12 <allisonstill@utexas.edu>
2025-05-05 12:59:01 -0500

    trying to build nextjs.yml

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,13 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {};
 
-export default nextConfig;
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+    output: 'export',  // Enables static HTML export
+    distDir: 'out',
+    images: {
+      unoptimized: true, // Required for static export
+    },
+    // Add basePath if you're not using a custom domain
+    // basePath: '/personalportfolio',
+  }
+  
+  module.exports = nextConfig
