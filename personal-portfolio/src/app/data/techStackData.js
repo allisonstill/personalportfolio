a1bc2fa6effa531a9e4af27b117ca2092b38109f
allistill12 <allisonstill@utexas.edu>
2025-05-05 12:28:35 -0500

    removing personal

--- personal-portfolio/src/app/data/techStackData.js
+++
@@ -1,9 +0,0 @@
-import { FaReact, FaJs, FaPython, FaNodeJs, FaGitAlt } from 'react-icons/fa';
-
-export const techStack = [
-  { name: "React", icon: <FaReact className="text-blue-400 text-2xl" /> },
-  { name: "JavaScript", icon: <FaJs className="text-yellow-400 text-2xl" /> },
-  { name: "Python", icon: <FaPython className="text-green-400 text-2xl" /> },
-  { name: "Node.js", icon: <FaNodeJs className="text-lime-400 text-2xl" /> },
-  { name: "Git", icon: <FaGitAlt className="text-orange-500 text-2xl" /> }
-];
